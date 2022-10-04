92412a8db1b2242ce38d53e371aeef768ce41b75
Nam1107 <teenhainam2603@gmail.com>
2022-10-05 01:04:00 +0700

    update

--- controllers/cartitem.php
+++ controllers/cartitem.php
@@ -5,36 +5,24 @@ require './helper/middleware.php';
 checkRequest('GET');
 $table = 'product';
 $res['status'] = 1;
-if (!isset($_GET['page']) || $_GET['page'] <= 0) {
-    $page = 1;
-} else {
-    $page = $_GET['page'];
-}
+
+$page = 1;
+
 
 $perPage = 10;
-if (isset($_GET['perPage'])) {
-    $perPage = $_GET['perPage'];
-}
+
 
 $search = '';
-if (isset($_GET['search'])) {
-    $search = $_GET['search'];
-}
+
 
 $searchType = 'name';
-if (isset($_GET['searchType'])) {
-    $searchType = $_GET['searchType'];
-}
+
 
 $orderBy = 'name';
-if (isset($_GET['orderBy'])) {
-    $orderBy = $_GET['orderBy'];
-}
+
 
 $orderType = 'ASC';
-if (isset($_GET['orderType'])) {
-    $orderType = $_GET['orderType'];
-}
+
 
 $condition = [
     "$searchType" => $search,
@@ -48,17 +36,7 @@ if ($page >= $check && $check > 0) {
     $page = $check - 1;
 }
 $obj = selectAll($table, $condition, " ORDER BY $orderBy $orderType LIMIT $perPage OFFSET $offset");
-$sizeArray = sizeof($obj) - 1;
-for ($i = 0; $i <= $sizeArray; $i++) {
-    if (currentTime() > $obj["$i"]['startSale'] && currentTime() < $obj["$i"]['endSale']) {
-        $obj["$i"]['saleStatus'] = 1;
-    } else $obj["$i"]['saleStatus'] = 0;
-    $gallery = selectAll('gallery', ['productID' => $obj["$i"]['ID']]);
-    $obj["$i"]["gallery"] = $gallery;
-    $category = selectOne('category', ['ID' => $obj["$i"]['categoryID']]);
-    unset($obj["$i"]['categoryID']);
-    $obj["$i"]['category'] = $category;
-}
+
 $totalCount = custom("SELECT COUNT(*)  AS totalCount FROM $table");
 
 $res['obj'] = $obj;
