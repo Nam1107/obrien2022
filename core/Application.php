dc749a7da09515bf590cd7ccf88fec88c4786021
Nam1107 <teenhainam2603@gmail.com>
2022-10-04 23:29:00 +0700

    push

--- core/Application.php
+++ core/Application.php
@@ -1,7 +1,7 @@
 <?php
 
-require './database/db.php';
-require './helper/middleware.php';
+// require './database/db.php';
+// require './helper/middleware.php';
 class Application
 {
     protected $controller;
@@ -13,44 +13,49 @@ class Application
 
         $arr = $this->UrlProcess();
         array_splice($arr, 0, 1);
-
-        if ($arr[0] == 'product') {
-            require_once './controllers/product.php';
-            if ($arr[1] == 'listproduct') {
-                // call_user_func('ListProduct', '');
-                // test();
-                ListProduct();
-            } elseif ($arr[1] == 'getProduct') {
-                getProduct();
-            } elseif ($arr[1] == 'test') {
-                test();
-            }
-            // if ($arr[1] == 'createProduct') {
-            //     createProduct();
-            // }
-            // if ($arr[1] == 'getProduct') {
-            //     getProduct()
-            // }
-            // if ($arr[1] == 'getProduct') {
-            //     getProduct()
-            // }
-            // if ($arr[1] == 'getProduct') {
-            //     getProduct()
-            // }
-        }
-
-
-
-        // if (file_exists("./controllers/" . $arr[0] . ".php")) {
-        //     $this->controller = $arr[0];
-        //     require_once "./controllers/" . $arr[0] . ".php";
-        //     if (isset($arr[1])) {
-        //         if (method_exists($this->controller, $arr[1])) {
-        //             $this->action = $arr[1];
-        //         }
-        //         call_user_func_array([$this->controller, $this->action], []);
-        //     }
+        // switch($arr[0]){
+        //     case 'product':
+        //         break;
+        //     case 'test':
+        //         break;
         // }
+        // if ($arr[0] == 'product') {
+        //     require_once './controllers/product.php';
+        //     if ($arr[1] == 'listproduct') {
+        //         // call_user_func('ListProduct', '');
+        //         // test();
+        //         ListProduct();
+        //     } elseif ($arr[1] == 'getProduct') {
+        //         getProduct();
+        //     } elseif ($arr[1] == 'test') {
+        //         test();
+        //     }
+        // if ($arr[1] == 'createProduct') {
+        //     createProduct();
+        // }
+        // if ($arr[1] == 'getProduct') {
+        //     getProduct()
+        // }
+        // if ($arr[1] == 'getProduct') {
+        //     getProduct()
+        // }
+        // if ($arr[1] == 'getProduct') {
+        //     getProduct()
+        // }
+        // }
+
+
+
+        if (file_exists("./controllers/" . $arr[0] . ".php")) {
+            $this->controller = $arr[0];
+            require "./controllers/" . $arr[0] . ".php";
+            if (isset($arr[1])) {
+                if (method_exists($this->controller, $arr[1])) {
+                    $this->action = $arr[1];
+                }
+                call_user_func_array([$this->controller, $this->action], []);
+            }
+        }
     }
 
     function UrlProcess()
