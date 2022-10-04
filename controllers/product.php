67b974f1c8504a3ca7c6855301519e29b5d35d97
Nam1107 <teenhainam2603@gmail.com>
2022-10-04 19:24:51 +0700

    update

--- controllers/product.php
+++ controllers/product.php
@@ -7,6 +7,49 @@
 function test()
 {
     checkRequest('GET');
+    $table = 'product';
+    $res['status'] = 1;
+    if (!isset($_GET['page']) || $_GET['page'] <= 0) {
+        $page = 1;
+    } else {
+        $page = $_GET['page'];
+    }
+
+    $perPage = 10;
+    if (isset($_GET['perPage'])) {
+        $perPage = $_GET['perPage'];
+    }
+
+    $search = '';
+    if (isset($_GET['search'])) {
+        $search = $_GET['search'];
+    }
+
+    $searchType = 'name';
+    if (isset($_GET['searchType'])) {
+        $searchType = $_GET['searchType'];
+    }
+
+    $orderBy = 'name';
+    if (isset($_GET['orderBy'])) {
+        $orderBy = $_GET['orderBy'];
+    }
+
+    $orderType = 'ASC';
+    if (isset($_GET['orderType'])) {
+        $orderType = $_GET['orderType'];
+    }
+
+    $condition = [
+        "$searchType" => $search,
+    ];
+
+    $offset = $perPage * ($page - 1);
+
+    $obj = selectAll($table, $condition, " ORDER BY $orderBy $orderType LIMIT $perPage OFFSET $offset");
+
+    $product['condition'] = $condition;
+    $product['pro'] = $obj;
     $product['obj'] = custom('select * from product');
     dd($product);
     exit;
